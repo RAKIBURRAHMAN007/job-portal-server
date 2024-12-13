00b7f10a592fa54d477b5b23ffa3add864739275
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2024-12-14 00:26:34 +0600

    geting job data complete

--- index.js
+++ index.js
@@ -30,9 +30,24 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    // job related apis
+
+    const jobsCollection = client.db('job-portal').collection('jobs');
+
+    app.get('/jobs',async(req,res)=>{
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
